8b3435a839b25167870f9849a1a7ad3a1f904be7
hyuk9 <107982323+hyuk9@users.noreply.github.com>
2022-11-10 21:55:59 +0900

    Test

---
+++ Previous_Test/SimpleDMS/src/main/java/com/example/simpledms/controller/EmpController.java
@@ -0,0 +1,168 @@
+package com.example.simpledms.controller;
+
+import com.example.simpledms.model.Dept;
+import com.example.simpledms.model.Emp;
+import com.example.simpledms.service.DeptService;
+import com.example.simpledms.service.EmpService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * packageName : com.example.jpaexam.controller.exam07
+ * fileName : Dept07Controller
+ * author : ds
+ * date : 2022-10-21
+ * description : 부서 컨트롤러 @RestController, ResponseEntity, getDeptAll()
+ * ===========================================================
+ * DATE            AUTHOR             NOTE
+ * —————————————————————————————
+ * 2022-10-21         ds          최초 생성
+ */
+// @RestController : return 값이 json 데이터 형태로 출력됨
+@Slf4j
+// CORS 보안 : 기본적으로 한사이트에서 포트를 달리 사용 못함
+// @CrossOrigin(허용할_사이트주소(vue 사이트주소 : 포트)) : CORS 보안을 허용해주는 어노테이션
+@CrossOrigin(origins = "http://localhost")
+@RestController
+@RequestMapping("/api")
+public class EmpController {
+
+    //    스프링부트 : DI(의존성 주입) ( @Autowired )
+    @Autowired
+    EmpService empService; // @Autowired : 스프링부트가 가동될때 생성된 객체를 하나 받아오기
+
+    //  *1) 클라이언트 : (form태그) Get 방식(url) -> 2) 서버 : @GetMapping("url") -> 3) DB: select 요청,
+    @GetMapping("/emp")
+    public ResponseEntity<Object> getEmpAll(@RequestParam(required = false) String ename) {
+
+        try {
+
+            List<Emp> list = Collections.emptyList();
+
+            if (ename == null){
+                list = empService.findAll();
+            }else {
+                list = empService.findAllByEnameContaining(ename);
+            }
+
+            if (list.isEmpty() == false) {
+//                데이터 + 성공 메세지 전송
+                return new ResponseEntity<>(list, HttpStatus.OK);
+            } else {
+//                데이터 없음 메세지 전송(클라이언트)
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+        } catch (Exception e) {
+            log.debug(e.getMessage());
+            // 서버에러 발생 메세지 전송(클라이언트)
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/emp/{eno}")
+    public ResponseEntity<Object> getEmpID(@PathVariable int eno) {
+
+        try {
+            Optional<Emp> optionalEmp = empService.findById(eno);
+
+            if (optionalEmp.isPresent() == true) {
+//                데이터 + 성공 메세지 전송
+                return new ResponseEntity<>(optionalEmp.get(), HttpStatus.OK);
+            } else {
+//                데이터 없음 메세지 전송(클라이언트)
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+        } catch (Exception e) {
+            log.debug(e.getMessage());
+            // 서버에러 발생 메세지 전송(클라이언트)
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    //  *1) 클라이언트 : (form태그) Get 방식(url) -> 2) 서버 : @GetMapping("url") -> 3) DB: select 요청,
+    @DeleteMapping("/emp/all")
+    public ResponseEntity<Object> removeAll() {
+
+        try {
+            empService.removeAll();
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            log.debug(e.getMessage());
+            // 서버에러 발생 메세지 전송(클라이언트)
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+    //  *1) 클라이언트 : (form태그) Get 방식(url) -> 2) 서버 : @GetMapping("url") -> 3) DB: select 요청,
+    @PostMapping("/emp")
+    public ResponseEntity<Object> createEmp(@RequestBody Emp emp) {
+
+        try {
+            Emp emp2 = empService.save(emp);
+
+            return new ResponseEntity<>(emp2, HttpStatus.OK);
+        } catch (Exception e) {
+            log.debug(e.getMessage());
+            // 서버에러 발생 메세지 전송(클라이언트)
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+    @PutMapping("/emp/{eno}")
+    public ResponseEntity<Object> updateEmp(@PathVariable int eno, @RequestBody Emp emp) {
+
+        try {
+            Emp emp2 = empService.save(emp);
+
+            return new ResponseEntity<>(emp2, HttpStatus.OK);
+        } catch (Exception e) {
+            log.debug(e.getMessage());
+            // 서버에러 발생 메세지 전송(클라이언트)
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+    @DeleteMapping("/emp/deletion/{eno}")
+    public ResponseEntity<Object> deleteId(@PathVariable int eno) {
+
+        try {
+            boolean bSuccess = empService.removeById(eno);
+
+            if (bSuccess == true) {
+//                데이터 + 성공 메세지 전송
+                return new ResponseEntity<>(HttpStatus.OK);
+            } else {
+//                데이터 없음 메세지 전송(클라이언트)
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+        } catch (Exception e) {
+            log.debug(e.getMessage());
+            // 서버에러 발생 메세지 전송(클라이언트)
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
